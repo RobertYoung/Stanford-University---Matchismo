4128d547fa57087b131e241373ea416b0a5f99c7
Robert Young <apple@Robs-MacBook-Pro.local>
2014-01-09 20:07:55 +0000

    Initial Commit

---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Robert Young on 09/01/2014.
+//  Copyright (c) 2014 Robert Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
